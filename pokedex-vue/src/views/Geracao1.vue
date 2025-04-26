1d2a4cdd2bac99fdee3fdb37d397b583557351bb
ftreitasyuri <yuriftq@gmail.com>
2025-04-26 15:40:58 -0300

    Upload: Subindo configuração da view Geracao1 com a congiguração da requisição na API

--- pokedex-vue/src/views/Geracao1.vue
+++ pokedex-vue/src/views/Geracao1.vue
@@ -1,3 +1,28 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+// Vetores
+let vetor = ref([]);
+
+onMounted(async () => {
+
+  for (let indice = 1; indice <= 151; indice++) {
+    let requisicao = await fetch(`https://pokeapi.co/api/v2/pokemon/${indice}`)
+    let pokemon = await requisicao.json();
+    vetor.value.push(pokemon);
+  }
+});
+</script>
+
 <template>
-    <h1>Primeira Geração</h1>
+  <h1>Primeira Geração</h1>
+
+  <ul>
+    <li v-for="v in vetor">
+      {{ v.name }}
+    </li>
+  </ul>
+
 </template>
+
+<!-- https://pokeapi.co/api/v2/pokemon/1 -->
