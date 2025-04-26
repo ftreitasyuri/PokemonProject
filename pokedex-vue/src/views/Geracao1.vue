0bc193a5b98e9bb39988e194008aa794b248165f
ftreitasyuri <yuriftq@gmail.com>
2025-04-26 15:52:02 -0300

    Upload: Subindo configuração para exibir as imagens e dados dos pokemons

--- pokedex-vue/src/views/Geracao1.vue
+++ pokedex-vue/src/views/Geracao1.vue
@@ -15,13 +15,24 @@ onMounted(async () => {
 </script>
 
 <template>
-  <h1>Primeira Geração</h1>
-
-  <ul>
+  <!-- <h1>Primeira Geração</h1> -->
+<!-- Debug -->
+  <!-- <ul>
     <li v-for="v in vetor">
       {{ v.name }}
     </li>
-  </ul>
+  </ul> -->
+
+  <main class="container ">
+    <div class="row">
+      <div class="col-12 col-sm-6 col-md-4 col-lg-3" v-for="v in vetor">
+        <div class="card">
+          <img :src="v.sprites.other.home.front_default" alt="Imagem do pokemon">
+          <p>{{ v.name }}</p>
+        </div>
+      </div>
+    </div> 
+  </main>
 
 </template>
 
