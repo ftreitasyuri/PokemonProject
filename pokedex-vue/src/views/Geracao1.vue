adf01e21fe2832d4a34f719ddf7e6ce0fcf166ff
ftreitasyuri <yuriftq@gmail.com>
2025-04-27 10:37:50 -0300

    Upload: Subindo fução que faz a filtragem de pokemons

--- pokedex-vue/src/views/Geracao1.vue
+++ pokedex-vue/src/views/Geracao1.vue
@@ -6,6 +6,9 @@ let vetor = ref([]);
 
 let carregamento = ref(true);
 
+// Variável para armazenar o termo da filtragem
+let termoFiltro = ref('');
+
 onMounted(async () => {
 
   for (let indice = 1; indice <= 151; indice++) {
@@ -17,6 +20,13 @@ onMounted(async () => {
   carregamento.value = false;
 
 });
+
+// Funções
+function filtrar(){
+  return vetor.value.filter(obj => obj.name.toLowerCase().includes(termoFiltro.value.toLowerCase()));
+}
+
+
 </script>
 
 <template>
@@ -33,8 +43,22 @@ onMounted(async () => {
   </div>
 
   <main class="container " v-if="!carregamento">
+
+     <!-- {{ termoFiltro }} -->
+    <!-- Filtro -->
     <div class="row">
-      <div class="col-12 col-sm-6 col-md-4 col-lg-3" v-for="v in vetor">
+      <div class="col-12">
+        <input type="text" v-model="termoFiltro" class="text form-control pesquisa" placeholder="Qual pokemon você está procurando">
+        <p v-if="filtrar().length == 0">Não foi encontrado nenhum pokemon</p>
+        <p v-else-if="filtrar().length == 1">Foi encontrado apenas 1 pokemon</p>
+        <p v-else>Foram encontrados {{ filtrar().length }} pokemons</p>
+      </div>
+    </div>
+
+    <!-- Listagem -->
+
+    <div class="row">
+      <div class="col-12 col-sm-6 col-md-4 col-lg-3" v-for="v in filtrar()">
         <div class="card" :class="v.types[0].type.name">
           <img :src="v.sprites.other.home.front_default" alt="Imagem do pokemon">
           <p>Name: {{ v.name }}</p>
